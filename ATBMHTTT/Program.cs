7bbe5d61530a895c79d0d2a501c3b42436e2cd47
npvinh2410 <npvinh2410@gmail.com>
2022-04-09 15:25:54 +0700

    Add project files.

---
+++ ATBMHTTT/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ATBMHTTT
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            //
+            OracleConnection conn = DBUtils.GetDBConnection();
+
+            Console.WriteLine("Get Connection: " + conn);
+            try
+            {
+                conn.Open();
+
+                Console.WriteLine(conn.ConnectionString, "Successful Connection");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("## ERROR: " + ex.Message);
+                Console.Read();
+                return;
+            }
+
+            Console.WriteLine("Connection successful!");
+
+            Console.Read();
+        }
+    }
+}
